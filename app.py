4c33b7f080c40c4b8b7f491639174c027ba18c46
Sima Saadi <106560902+simasaadi@users.noreply.github.com>
2025-12-12 16:26:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
-
 # -----------------------------
 # App config + minimal styling
 # -----------------------------
